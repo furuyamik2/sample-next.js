84c0d4fe77fac27ab26d5bf0b2041bf55ceb81bd
furuyamik2 <furuya002.3020junjun@gmail.com>
2025-02-03 20:58:56 +0900

    create nextapp

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="flex">
+      <div className="bg-cyan-300">Item1</div>
+      <div className="bg-gray-500">Item2</div>
+      <div className="bg-lime-400">Item3</div>
+      <div className="bg-purple-300">Item4</div>
     </div>
   );
 }
