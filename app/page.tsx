2f522564419125bd0945c87e960e19fa971cd8c6
furuyamik2 <furuya002.3020junjun@gmail.com>
2025-02-03 20:47:56 +0900

    create nextapp

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex gap-4 mx-4">
+      <div className="w-1/2 flex flex-col gap-1">
+        <div className="bg-green-400">Column Left Row 1</div>
+        <div className="bg-green-400">Column Left Row 2</div>
+        <div className="bg-green-400">Column Left Row 3</div>
+      </div>
+      <div className="w-1/2  flex flex-col gap-1">
+        <div className="bg-blue-500">Column Right Row 1</div>
+        <div className="bg-blue-500">Column Right Row 2</div>
+        <div className="bg-blue-500">Column Right Row 3</div>
+      </div>
     </div>
   );
 }
